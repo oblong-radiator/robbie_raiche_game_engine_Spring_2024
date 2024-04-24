b0fa045b60d71983220d3bb78ec011a8d7dfa87b
oblong-radiator <160270209+oblong-radiator@users.noreply.github.com>
2024-04-24 14:13:46 -0700

    trying to fix roomba image

--- sprites.py
+++ sprites.py
@@ -155,7 +155,7 @@ class Enemy(Sprite): # dis a copy of the other class
         Sprite.__init__(self, self.groups)
         self.game = game
         # Load the image file for the enemy
-        self.image = pg.image.load(path.join(img_folder, "roomba.jpg")).convert()
+        self.image = pg.image.load(path.join(img_folder, "roomba.png")).convert()
         self.rect = self.image.get_rect()
         self.x = x * s.TILESIZE
         self.y = y * s.TILESIZE
